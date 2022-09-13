f30213f43baa39cd646de0f28e396a259e918a83
Nikita <nikitazapletin@gmail.com>
2022-09-14 01:44:57 +0300

    tabs + animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,43 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener(`DOMContentLoaded`, () => {
+  const tabs = document.querySelectorAll(`.tabheader__item`),
+        tabsContent = document.querySelectorAll(`.tabcontent`),
+        tabsParent = document.querySelector(`.tabheader__items`);
 
+  function hideTabContetn() {
+    tabsContent.forEach(item => {
+      item.classList.add(`hide`);
+      item.classList.remove(`show`, `fade`);
+    });
+    tabs.forEach(tab => {
+      tab.classList.remove(`tabheader__item_active`);
+    });
+  }
+
+  function showTabContetn() {
+    let i = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+    tabsContent[i].classList.add(`show`, `fade`);
+    tabsContent[i].classList.remove(`hide`);
+    tabs[i].classList.add(`tabheader__item_active`);
+  }
+
+  hideTabContetn();
+  showTabContetn();
+  tabsParent.addEventListener(`click`, event => {
+    const target = event.target;
+
+    if (target && target.classList.contains(`tabheader__item`)) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContetn();
+          showTabContetn(i);
+        }
+      });
+    }
+  });
+});
 
 /***/ })
 
