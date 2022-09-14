79efcffc5b0af84981c19812de31c834bb192a02
Nikita <nikitazapletin@gmail.com>
2022-09-15 02:11:27 +0300

    Add rest element
    In menuCard class add rest element for other classes wich wil be add to menuCard element

--- dist/js/script.js
+++ dist/js/script.js
@@ -239,6 +239,12 @@ window.addEventListener(`DOMContentLoaded`, () => {
       this.title = title;
       this.descr = descr;
       this.price = price;
+
+      for (var _len = arguments.length, classes = new Array(_len > 6 ? _len - 6 : 0), _key = 6; _key < _len; _key++) {
+        classes[_key - 6] = arguments[_key];
+      }
+
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 60; //Курс рубля
 
@@ -252,16 +258,22 @@ window.addEventListener(`DOMContentLoaded`, () => {
     render() {
       //Формирование верстки обычно называют так
       const element = document.createElement(`div`);
+
+      if (this.classes.length === 0) {
+        this.element = `menu__item`;
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-                <div class = "menu__item">
-                    <img src =${this.src} alt =${this.alt}>
-                    <h3 class = "menu__item-subtitle">${this.title}</h3>
-                    <div class = "menu__item-descr">${this.descr}</div>
-                    <div class = "menu__item-divider"></div>
-                    <div class = "menu__item-price">
-                        <div class = "menu__item-cost"> Цена: </div>
-                        <div class = "menu__item-total"><span>${this.price}</span> руб/день </div>
-                    </div>
+                <img src =${this.src} alt =${this.alt}>
+                <h3 class = "menu__item-subtitle">${this.title}</h3>
+                <div class = "menu__item-descr">${this.descr}</div>
+                <div class = "menu__item-divider"></div>
+                <div class = "menu__item-price">
+                    <div class = "menu__item-cost"> Цена: </div>
+                    <div class = "menu__item-total"><span>${this.price}</span> руб/день </div>
                 </div>
             `;
       this.parent.append(element);
@@ -269,10 +281,10 @@ window.addEventListener(`DOMContentLoaded`, () => {
 
   }
 
-  new MenuCard("img/tabs/vegy.jpg", "vegy", `Меню "Фитнес"`, `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`, 9, `.menu .container`).render(); // Если объект используется один раз то такой вариант
+  new MenuCard("img/tabs/vegy.jpg", "vegy", `Меню "Фитнес"`, `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`, 9, `.menu .container`, `menu__item`, `big`).render(); // Если объект используется один раз то такой вариант
 
-  new MenuCard("img/tabs/elite.jpg", "elite", `Меню “Премиум”`, `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`, 14, `.menu .container`).render();
-  new MenuCard("img/tabs/post.jpg", "post", `Меню "Постное"`, `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`, 21, `.menu .container`).render();
+  new MenuCard("img/tabs/elite.jpg", "elite", `Меню “Премиум”`, `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`, 14, `.menu .container`, `menu__item`).render();
+  new MenuCard("img/tabs/post.jpg", "post", `Меню "Постное"`, `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`, 21, `.menu .container`, `menu__item`).render();
 });
 
 /***/ })
