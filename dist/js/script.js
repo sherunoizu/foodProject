4d0f6ead2710b214f097ad27fceb78a5c0441a14
Nikita <nikitazapletin@gmail.com>
2022-09-17 04:06:37 +0300

    add slider animation
    Realise carousel animation with nativeJS

--- dist/js/script.js
+++ dist/js/script.js
@@ -388,10 +388,90 @@ window.addEventListener(`DOMContentLoaded`, () => {
       prevModalDialog.classList.remove('hide');
       closeModal();
     }, 5000);
-  } // fetch('http://localhost:3000/menu')
-  //     .then(data => data.json())
-  //     .then(res => console.log(res));
+  } //Создание слайдера (Карусель)
 
+
+  const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width; // Получаем примененные стили CSS
+  // Для этого слайдера нам понадобится в HTML создать внутри обертки слайдеров ещё одну
+  // slider-inner. Wrapper будет служить окном, через которое мы можем видеть весь
+  // контент, подходящий под нужную ширину.
+  // slider-inner - будет занимать ширину = колво слайдов * ширину слайда
+  // При нажатии кнопок, мы будем передвигать inner относительно wrapper'a на
+  // нужное колво пикселей (ширина одного слайда)
+
+  let slideIndex = 1;
+  let offset = 0; // отступ
+
+  totalInit();
+
+  function totalInit() {
+    if (slides.length < 10) {
+      total.textContent = `0${slides.length}`;
+      current.textContent = `0${slideIndex}`;
+    } else {
+      total.textContent = slides.length;
+      current.textContent = slideIndex;
+    }
+  }
+
+  slidesField.style.width = 100 * slides.length + `%`;
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+  slidesWrapper.style.overflow = 'hidden';
+  slides.forEach(slide => {
+    slide.style.width = width; // Устанавливаем одинаковую ширину для всех слайдов
+  });
+  next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      // width = '500px' - необходимо превратить в числовой тип данных
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+      slideIndex = 1;
+    } else {
+      slideIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = `${slideIndex}`;
+    }
+  });
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      // width = '500px' - необходимо превратить в числовой тип данных
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = `${slideIndex}`;
+    }
+  });
 });
 
 /***/ })
